d51b022b7674d6909fc2f30ae0bf66b7f5c4033d
Kim Konstantin <k.konstantin.27.21@gmail.com>
2024-02-18 22:02:31 +1000

    add StringsStatisticsService

---
+++ src/main/java/services/statisticsServices/StringsStatisticsService.java
@@ -0,0 +1,37 @@
+package services.statisticsServices;
+
+import utils.SizeUnitUtil;
+
+public class StringsStatisticsService extends BaseStatisticsService {
+    private int shortestLineLength = Integer.MAX_VALUE;
+    private long shortestLineSize;
+    private int longestLineLength = Integer.MIN_VALUE;
+    private long longestLineSize;
+
+    public void calculateStatistics(String line, boolean isFullStatistics) {
+        numberOfLines++;
+        if (isFullStatistics) {
+            if (line.length() < shortestLineLength) {
+                shortestLineLength = line.length();
+                shortestLineSize = line.getBytes().length;
+            }
+            if (line.length() > longestLineLength) {
+                longestLineLength = line.length();
+                longestLineSize = line.getBytes().length;
+            }
+        }
+    }
+
+    public void printStatistics(boolean isFullStatistics) {
+        df.setMaximumFractionDigits(2);
+        System.out.println("Количество строк: " + df.format(numberOfLines));
+        if (isFullStatistics) {
+            SizeUnitUtil sizeUnitUtil = new SizeUnitUtil();
+            System.out.println("Размер самой короткой строки: " + sizeUnitUtil.convertToBiggerUnits(shortestLineSize));
+            System.out.println("Размер самой длинной строки: " + sizeUnitUtil.convertToBiggerUnits(longestLineSize));
+            System.out.println("--------------------------------------------------");
+            String s = new String(new char[1029129]).replace('\0', 'A');
+            System.out.println(sizeUnitUtil.convertToBiggerUnits(s.getBytes().length));
+        }
+    }
+}
