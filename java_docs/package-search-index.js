42dfdfc3350ca6eb8c6138dbf698f31edabe7f49
Kim Konstantin <k.konstantin.27.21@gmail.com>
2024-02-23 07:38:50 +1000

    update java docs

---
+++ java_docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"entities"},{"l":"entitiesTests"},{"l":"services"},{"l":"servicesTests"},{"l":"utils"},{"l":"utilTests"}];updateSearchResults();
