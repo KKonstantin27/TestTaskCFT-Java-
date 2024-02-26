f032ab1e997c60a3c1a605ff880d463139f0403a
Kim Konstantin <k.konstantin.27.21@gmail.com>
2024-02-27 09:42:44 +1000

    refactor LineTypeUtilTest (change tested methods)

--- src/test/java/utilTests/LineTypeUtilTest.java
+++ src/test/java/utilTests/LineTypeUtilTest.java
@@ -1,58 +1,41 @@
 package utilTests;
 
+import enums.TypeOfLine;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import utils.LineTypeUtil;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LineTypeUtilTest {
     private final LineTypeUtil lineTypeUtil = new LineTypeUtil();
 
     @Test
     @Order(1)
-    public void isFloatsShouldReturnTrueWhenArgumentIsFloat() {
-        assertTrue(lineTypeUtil.isFloat("3.1415"));
-        assertTrue(lineTypeUtil.isFloat(".1415"));
-        assertTrue(lineTypeUtil.isFloat("1.528535047E-25"));
-        assertTrue(lineTypeUtil.isFloat("-1.528535047E-25"));
-        assertTrue(lineTypeUtil.isFloat("0.0"));
+    public void floatsLineShouldReturnFloatsType() {
+        assertEquals(TypeOfLine.FLOAT, lineTypeUtil.defineTypeOfLine("3.1415"));
+        assertEquals(TypeOfLine.FLOAT, lineTypeUtil.defineTypeOfLine(".1415"));
+        assertEquals(TypeOfLine.FLOAT, lineTypeUtil.defineTypeOfLine("1.528535047E-25"));
+        assertEquals(TypeOfLine.FLOAT, lineTypeUtil.defineTypeOfLine("-1.528535047E-25"));
+        assertEquals(TypeOfLine.FLOAT, lineTypeUtil.defineTypeOfLine("0.0"));
     }
 
     @Test
     @Order(2)
-    public void isFloatsShouldReturnFalseWhenArgumentIsNotFloat() {
-        assertFalse(lineTypeUtil.isFloat("12345"));
-        assertFalse(lineTypeUtil.isFloat("0"));
-        assertFalse(lineTypeUtil.isFloat("1234567890123456789"));
-        assertFalse(lineTypeUtil.isFloat("Test String"));
-        assertFalse(lineTypeUtil.isFloat(""));
-        assertFalse(lineTypeUtil.isFloat(" "));
-        assertFalse(lineTypeUtil.isFloat("/?{"));
-        assertFalse(lineTypeUtil.isFloat("."));
+    public void integersLineShouldReturnIntegersType() {
+        assertEquals(TypeOfLine.INTEGER, lineTypeUtil.defineTypeOfLine("1234567890123456789"));
+        assertEquals(TypeOfLine.INTEGER, lineTypeUtil.defineTypeOfLine("-1234567890123456789"));
+        assertEquals(TypeOfLine.INTEGER, lineTypeUtil.defineTypeOfLine("12345"));
+        assertEquals(TypeOfLine.INTEGER, lineTypeUtil.defineTypeOfLine("0"));
     }
 
     @Test
     @Order(3)
-    public void isIntegerShouldReturnTrueWhenArgumentIsInteger() {
-        assertTrue(lineTypeUtil.isInteger("1234567890123456789"));
-        assertTrue(lineTypeUtil.isInteger("-1234567890123456789"));
-        assertTrue(lineTypeUtil.isInteger("12345"));
-        assertTrue(lineTypeUtil.isInteger("0"));
-    }
-
-    @Test
-    @Order(4)
-    public void isIntegerShouldReturnFalseWhenArgumentIsNotInteger() {
-        assertFalse(lineTypeUtil.isInteger("3.1415"));
-        assertFalse(lineTypeUtil.isInteger(".12345"));
-        assertFalse(lineTypeUtil.isInteger("0.0"));
-        assertFalse(lineTypeUtil.isInteger("1.528535047E-25"));
-        assertFalse(lineTypeUtil.isInteger("Test String"));
-        assertFalse(lineTypeUtil.isInteger(""));
-        assertFalse(lineTypeUtil.isInteger(" "));
-        assertFalse(lineTypeUtil.isInteger("/?{"));
-        assertFalse(lineTypeUtil.isInteger("."));
+    public void stringsLineShouldReturnStringsType() {
+        assertEquals(TypeOfLine.STRING, lineTypeUtil.defineTypeOfLine("Test string"));
+        assertEquals(TypeOfLine.STRING, lineTypeUtil.defineTypeOfLine("Тестовая строка"));
+        assertEquals(TypeOfLine.STRING, lineTypeUtil.defineTypeOfLine(" "));
+        assertEquals(TypeOfLine.STRING, lineTypeUtil.defineTypeOfLine("/?{"));
+        assertEquals(TypeOfLine.STRING, lineTypeUtil.defineTypeOfLine("."));
     }
 }
